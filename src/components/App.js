868242189e779059cef56dd2a3e097589ba4bb40
Sungmiii <christyksmile@gmail.com>
2019-05-09 16:34:46 +1200

    set state

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 class App extends React.Component {
+    state = { language: 'english' }
+
+    onLanguageChange = (lang) => {
+        this.setState({
+            language: lang
+        })
+    };
+
     render() {
         return (
             <div className="ui container">
-                Apps
+                <div>
+                    Select a language:
+                    <i
+                        className="flag nz"
+                        onClick={() => this.onLanguageChange('english')}
+                    />
+                    <i
+                        className="flag kr"
+                        onClick={() => this.onLanguageChange('korean')}
+                    />
+                </div>
+                {this.state.language}
             </div>
         )
     }
